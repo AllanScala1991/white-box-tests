6b15d803375f92516fe13a351177f74e07d6660f
Allan Scala <allanscala@gmail.com>
2023-01-24 15:15:15 -0300

    feat: create task dto

---
+++ src/main/java/com/example/tasks/dtos/TaskDTO.java
@@ -0,0 +1,20 @@
+package com.example.tasks.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TaskDTO {
+
+    @NotBlank
+    String name;
+
+    @NotBlank
+    String description;
+
+    @NotBlank
+    Priority priority;
+
+}
